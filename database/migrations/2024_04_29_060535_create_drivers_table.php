7b2545ff900adcd81e4361e704caf6deb279e79b
Alireza Azizi <dev.azizimail@gmail.com>
2024-04-29 10:50:19 +0430

    create basic migration of project

---
+++ database/migrations/2024_04_29_060535_create_drivers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->unsignedBigInteger('driver_license_id');
+            $table->unsignedBigInteger('national_code');
+            $table->unsignedInteger('active_city_id');
+            $table->boolean('is_verified');
+            $table->boolean('is_banned');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
